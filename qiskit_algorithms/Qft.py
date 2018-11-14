3aef22f99d52c2061d2915648f96a49f4cee6ad1
Carsten Blank <blank@data-cybernetics.com>
2018-11-14 09:55:15 +0200

    Changing to Apache 2.0 Licence and adding a note to each file.

--- qiskit_algorithms/Qft.py
+++ qiskit_algorithms/Qft.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, Carsten Blank.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+
 import math
 from typing import Tuple, List
 
