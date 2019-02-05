c7f7790769b9b79f4e5e555def2ac858cb72cca1
Carsten Blank <blank@data-cybernetics.com>
2019-02-05 10:40:32 +0100

    Added more test cases in different tests plus refactoring.

--- tests/test_draper_adder.py
+++ tests/test_draper_adder.py
@@ -20,6 +20,8 @@ import logging
 
 import qiskit
 from ddt import ddt, data as test_data, unpack
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.extensions import standard
 
 from dc_qiskit_algorithms.draper_adder import draper_adder
 
@@ -31,36 +33,20 @@ LOG = logging.getLogger('test_DraperAdder')
 class DraperAdderTest(unittest.TestCase):
     """Test class for the systematic test of the Draper adder."""
 
-    @test_data(
-        (0, 0, None), (0, 1, None), (0, 2, None), (0, 3, None),
-        (1, 0, None), (1, 1, None), (1, 2, None), (1, 3, None),
-        (2, 0, None), (2, 1, None), (2, 2, None), (2, 3, None),
-        (3, 0, None), (3, 1, None), (3, 2, None), (3, 3, None),
-        (0, 0, 2), (0, 1, 2), (0, 2, 2), (0, 3, 2),
-        (1, 0, 2), (1, 1, 2), (1, 2, 2), (1, 3, 2),
-        (2, 0, 2), (2, 1, 2), (2, 2, 2), (2, 3, 2),
-        (3, 0, 2), (3, 1, 2), (3, 2, 2), (3, 3, 2),
-        (7, 0, None), (7, 1, None), (7, 2, None),
-        (7, 3, None), (7, 4, None), (7, 5, None),
-        (7, 6, None), (7, 7, None)
-    )
-    @unpack
-    def test_two_bit_adder(self, a_input, b_input, length):
-        # type: (DraperAdderTest, int, int, int) -> None
+    def _test(self, a_input, b_input, q_circuit, modulo):
         """
-        Test the addition of a and b. The length is used to override the
-        automatic qubit size calculation.
+        Test the addition of a and b.
 
         :param a_input: an integer
         :param b_input: an integer
-        :param length: a fixed qubit register size for both inputs.
+        :param q_circuit: the circuit to test
+        :param modulo: must be used for testing
         :return: nothing
         """
+        from qiskit import compile as q_compile
+
         LOG.info("Testing 'DraperAdder' with a=%d(%s), b=%d(%s).",
                  a_input, "{0:b}".format(a_input), b_input, "{0:b}".format(b_input))
-        q_circuit, modulo = draper_adder(a_input, b_input, length)
-
-        from qiskit import compile as q_compile
 
         backend = qiskit.Aer.get_backend('qasm_simulator')
         qobj = q_compile([q_circuit], backend=backend, shots=8192)
@@ -76,6 +62,61 @@ class DraperAdderTest(unittest.TestCase):
         data = result_list[0]  # type: dict
         self.assertEqual(int(data['b'], 2), b_input, "Register b must be unchanged!")
         self.assertEqual(int(data['a'], 2), (a_input + b_input) % modulo, "Addition must be correctly performed!")
+
+    @test_data(
+        (0, 0), (0, 1), (0, 2), (0, 3),
+        (1, 0), (1, 1), (1, 2), (1, 3),
+        (2, 0), (2, 1), (2, 2), (2, 3),
+        (3, 0), (3, 1), (3, 2), (3, 3),
+        (7, 0), (7, 1), (7, 2),
+        (7, 3), (7, 4), (7, 5),
+        (7, 6), (7, 7)
+    )
+    @unpack
+    def test_with_length_none(self, a_input, b_input):
+        # type: (DraperAdderTest, int, int) -> None
+        q_circuit, modulo = draper_adder(a_input, b_input, None)
+        self._test(a_input, b_input, q_circuit, modulo)
+
+    @test_data(
+        (0, 0), (0, 1), (0, 2), (0, 3),
+        (1, 0), (1, 1), (1, 2), (1, 3),
+        (2, 0), (2, 1), (2, 2), (2, 3),
+        (3, 0), (3, 1), (3, 2), (3, 3)
+    )
+    @unpack
+    def test_with_two_bits(self, a_input, b_input):
+        # type: (DraperAdderTest, int, int) -> None
+        q_circuit, modulo = draper_adder(a_input, b_input, 2)
+        self._test(a_input, b_input, q_circuit, modulo)
+
+    @test_data((7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7))
+    @unpack
+    def test_with_three_bits(self, a_input, b_input):
+        # type: (DraperAdderTest, int, int) -> None
+        q_circuit, modulo = draper_adder(a_input, b_input, 3)
+        self._test(a_input, b_input, q_circuit, modulo)
+
+    @test_data((7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7))
+    @unpack
+    def test_with_three_bits_with_barriers(self, a_input, b_input):
+        # type: (DraperAdderTest, int, int) -> None
+        q_circuit, modulo = draper_adder(a_input, b_input, 3, with_barriers=True)
+        self._test(a_input, b_input, q_circuit, modulo)
+
+    @test_data((7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7))
+    @unpack
+    def test_with_three_bits_with_pre_circuit(self, a_input, b_input):
+        # type: (DraperAdderTest, int, int) -> None
+        a_reg = QuantumRegister(3, "a")
+        b_reg = QuantumRegister(3, "b")
+        pre_q_circuit = QuantumCircuit(a_reg, b_reg, name="pre-circuit")
+        standard.x(pre_q_circuit, a_reg)
+        standard.x(pre_q_circuit, b_reg)
+        standard.x(pre_q_circuit, a_reg)
+        standard.x(pre_q_circuit, b_reg)
+        q_circuit, modulo = draper_adder(a_input, b_input, 3, pre_circuit=pre_q_circuit)
+        self._test(a_input, b_input, q_circuit, modulo)
 
 
 if __name__ == '__main__':
